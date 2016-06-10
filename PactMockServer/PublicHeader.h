bc5cf708abad72fbc4fecbdc10f5fd754df71ddd
Andrew Spinks <andrewspinks@gmail.com>
2016-06-10 11:07:33 +0900

    Add tests around mock server.

--- PactMockServer/PublicHeader.h
+++ PactMockServer/PublicHeader.h
@@ -11,7 +11,7 @@
 
 int32_t create_mock_server(const char *pact);
 bool mock_server_matched(int32_t port);
-char* mock_server_mismatches(int32_t port);
+const char *mock_server_mismatches(int32_t port);
 bool cleanup_mock_server(int32_t port);
 int32_t write_pact_file(int32_t port);
 
--- PactMockServerTests/PactMockServerTests.swift
+++ PactMockServerTests/PactMockServerTests.swift
@@ -10,27 +10,91 @@ import XCTest
 @testable import PactMockServer
 
 class PactMockServerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+  let pact = "{\n" +
+  "\"provider\": {\n" +
+  "  \"name\": \"Alice Service\"\n" +
+  "},\n" +
+  "\"consumer\": {\n" +
+  "  \"name\": \"Consumer\"\n" +
+  "},\n" +
+  "\"interactions\": [\n" +
+  "  {\n" +
+  "    \"description\": \"a retrieve Mallory request\",\n" +
+  "    \"request\": {\n" +
+  "      \"method\": \"GET\",\n" +
+  "      \"path\": \"/mallory\",\n" +
+  "      \"query\": \"name=ron&status=good\"\n" +
+  "    },\n" +
+  "    \"response\": {\n" +
+  "      \"status\": 200,\n" +
+  "      \"headers\": {\n" +
+  "        \"Content-Type\": \"text/html\"\n" +
+  "      },\n" +
+  "      \"body\": \"\\\"That is some good Mallory.\\\"\"\n" +
+  "    }\n" +
+  "  }\n" +
+  "],\n" +
+  "\"metadata\": {\n" +
+  "  \"pact-specification\": {\n" +
+  "    \"version\": \"1.0.0\"\n" +
+  "  },\n" +
+  "  \"pact-jvm\": {\n" +
+  "    \"version\": \"1.0.0\"\n" +
+  "  }\n" +
+  "}\n" +
+  "}\n"
+
+
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  func testMatchingExample() {
+    let port = PactMockServer.create_mock_server(pact)
+    print("starting test on port \(port)")
+  
+    let url = NSURL(string: "http://localhost:\(port)/mallory?name=ron&status=good")
+    let expectation = expectationWithDescription("Swift Expectations")
+
+    let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+      print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+      
+      XCTAssertTrue(PactMockServer.mock_server_matched(port))
+      
+      PactMockServer.cleanup_mock_server(port)
+      expectation.fulfill()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    task.resume()
+    waitForExpectationsWithTimeout(5.0, handler:nil)
+  }
+
+  func testMismatchExample() {
+    let port = PactMockServer.create_mock_server(pact)
+    print("starting test on port \(port)")
+    let url = NSURL(string: "http://localhost:\(port)/mallory?name=ron&status=NoGood")
+    let expectation = expectationWithDescription("Swift Expectations")
+
+    let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+      print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+      
+      XCTAssertFalse(PactMockServer.mock_server_matched(port))
+      let mismatchJson = String.fromCString(PactMockServer.mock_server_mismatches(port))
+      print("-----------Mismatches!--------")
+      print(mismatchJson)
+      print("------------------------------")
+      
+      PactMockServer.cleanup_mock_server(port)
+      expectation.fulfill()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
+    task.resume()
+    waitForExpectationsWithTimeout(5.0, handler:nil)
+  }
+
 }
