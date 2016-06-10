7860674cd335d50c42375329b113b69401f9aa16
Andrew Spinks <andrewspinks@gmail.com>
2016-06-10 09:05:06 +0900

    Initial Commit

---
+++ PactMockServer/PactMockServer.h
@@ -0,0 +1,19 @@
+//
+//  PactMockServer.h
+//  PactMockServer
+//
+//  Created by Andrew Spinks on 10/06/2016.
+//  Copyright © 2016 Pact. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PactMockServer.
+FOUNDATION_EXPORT double PactMockServerVersionNumber;
+
+//! Project version string for PactMockServer.
+FOUNDATION_EXPORT const unsigned char PactMockServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PactMockServer/PublicHeader.h>
+
+
